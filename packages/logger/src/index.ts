3bc28aebad93999284ebb256f77d8a1491dab628
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2024-04-30 09:56:39 +0200

    Bumped `@pixelic/logger` to `v0.0.4`

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,5 +1,5 @@
 /**
- * Contains all relevant exports of the logger module
+ * Contains all main Exports of the Logger Module
  *
  * @module
  */
@@ -7,64 +7,159 @@
 import chalk from "chalk";
 
 /**
- * Log Levels supported by the package sorted from most important to least important
+ * Log Levels supported by the package sorted from last important to most important
+ *
+ * @enum
  */
-type LogLevel = "CRITICAL" | "ERROR" | "WARNING" | "INFO" | "DEBUG";
+export enum LogLevels {
+  TRACE = 1,
+  DEBUG = 2,
+  INFO = 3,
+  WARNING = 4,
+  ERROR = 5,
+  CRITICAL = 6,
+}
 
 /**
- * Logs a message to the console using the runtime's default console implementation
+ * Union of Log Levels supported by the package
  *
- * @param service Service that emitted the log call
- * @param msg Message to log
- * @param level Log Level
+ * @type
  */
-const log = (service: string, msg: string, level: LogLevel): void => {
-  const chalkedDate = `${chalk.white(`${String(new Date().getHours()).padStart(2, "0")}:${String(new Date().getMinutes()).padStart(2, "0")}:${String(new Date().getSeconds()).padStart(2, "0")}`)}`;
-  if (level == "CRITICAL" || level == "ERROR") {
-    console.error(`[${chalkedDate}] [${chalk.green(service)}] » ${chalk.red(msg)}`);
-  } else if (level === "WARNING") {
-    console.warn(`[${chalkedDate}] [${chalk.green(service)}] » ${chalk.yellow(msg)}`);
-  } else if (level === "INFO") {
-    console.log(`[${chalkedDate}] [${chalk.green(service)}] » ${chalk.blue(msg)}`);
-  } else {
-    console.log(`[${chalkedDate}] [${chalk.green(service)}] » ${chalk.gray(msg)}`);
-  }
+export type LogLevel = keyof typeof LogLevels;
+
+/**
+ * Logger Config used when creating a new Logger Instance
+ *
+ * @type
+ */
+export type LoggerConfig = {
+  /**
+   * Minimum Log Level that should be logged to the console using the runtime's default console implementation
+   *
+   * @default "INFO"
+   */
+  minLevel?: LogLevel;
 };
 
-export default {
+/**
+ * Creates a new Logger Instance
+ *
+ * @param config
+ *
+ * @returns Logger Instance
+ */
+export const createLogger = (config?: LoggerConfig) => new Logger(config);
+
+/**
+ * Logger Instance
+ *
+ * @class
+ */
+class Logger {
+  private minLevel: LogLevel = "INFO";
+
   /**
-   * Logs a message to the console using the runtime's default console implementation with the **CRITICAL** Level
+   * @constructor Creates a Logger Instance
+   */
+  constructor(config?: LoggerConfig) {
+    if (config?.minLevel) this.minLevel = config.minLevel;
+  }
+
+  /**
+   * @private Checks whether the given Log Level satisfies the set `minLevel` of the constructor
+   *
+   * @param level Log Level to check against the `minLevel`
+   */
+  private satisfiesMinLevel(level: LogLevel): boolean {
+    return LogLevels[level] >= LogLevels[this.minLevel];
+  }
+
+  /**
+   * @private Logs a message to the console using the runtime's default console implementation
+   *
+   * @param service Service that emitted the log call
+   * @param msg Message to log
+   * @param level Log Level
+   */
+  private log(service: string, msg: string, level: LogLevel): void {
+    if (!this.satisfiesMinLevel(level)) return;
+
+    const chalkedDate = chalk.white(`${String(new Date().getHours()).padStart(2, "0")}:${String(new Date().getMinutes()).padStart(2, "0")}:${String(new Date().getSeconds()).padStart(2, "0")}`);
+
+    switch (level) {
+      case "CRITICAL":
+        console.error(`[${chalkedDate}] [${chalk.greenBright(service)}] [${chalk.bgRed(level)}] » ${chalk.bgRed(msg)}`);
+        break;
+      case "ERROR":
+        console.error(`[${chalkedDate}] [${chalk.greenBright(service)}] [${chalk.red(level)}] » ${chalk.red(msg)}`);
+        break;
+      case "WARNING":
+        console.warn(`[${chalkedDate}] [${chalk.greenBright(service)}] [${chalk.yellow(level)}] » ${chalk.yellow(msg)}`);
+        break;
+      case "INFO":
+        console.log(`[${chalkedDate}] [${chalk.greenBright(service)}] [${chalk.blue(level)}] » ${chalk.blue(msg)}`);
+        break;
+      case "DEBUG":
+        console.log(`[${chalkedDate}] [${chalk.greenBright(service)}] [${chalk.cyan(level)}] » ${chalk.cyan(msg)}`);
+        break;
+      case "TRACE":
+        console.log(`[${chalkedDate}] [${chalk.greenBright(service)}] [${chalk.gray(level)}] » ${chalk.gray(msg)}`);
+        break;
+    }
+  }
+
+  /**
+   * @public Logs a message to the console using the runtime's default console implementation with the **CRITICAL** Level
    *
    * @param service Service that emitted the log call
    * @param msg Message to log
    */
-  critical: (service: string, msg: string): void => log(service, msg, "CRITICAL"),
+  public critical(service: string, msg: string): void {
+    this.log(service, msg, "CRITICAL");
+  }
   /**
-   * Logs a message to the console using the runtime's default console implementation with the **ERROR** Level
+   * @public Logs a message to the console using the runtime's default console implementation with the **ERROR** Level
    *
    * @param service Service that emitted the log call
    * @param msg Message to log
    */
-  error: (service: string, msg: string): void => log(service, msg, "ERROR"),
+  public error(service: string, msg: string): void {
+    this.log(service, msg, "ERROR");
+  }
   /**
-   * Logs a message to the console using the runtime's default console implementation with the **WARNING** Level
+   * @public Logs a message to the console using the runtime's default console implementation with the **WARNING** Level
    *
    * @param service Service that emitted the log call
    * @param msg Message to log
    */
-  warning: (service: string, msg: string): void => log(service, msg, "WARNING"),
+  public warning(service: string, msg: string): void {
+    this.log(service, msg, "WARNING");
+  }
   /**
-   * Logs a message to the console using the runtime's default console implementation with the **INFO** Level
+   * @public Logs a message to the console using the runtime's default console implementation with the **INFO** Level
    *
    * @param service Service that emitted the log call
    * @param msg Message to log
    */
-  info: (service: string, msg: string): void => log(service, msg, "INFO"),
+  public info(service: string, msg: string): void {
+    this.log(service, msg, "INFO");
+  }
   /**
-   * Logs a message to the console using the runtime's default console implementation with the **DEBUG** Level
+   * @public Logs a message to the console using the runtime's default console implementation with the **DEBUG** Level
    *
    * @param service Service that emitted the log call
    * @param msg Message to log
    */
-  debug: (service: string, msg: string): void => log(service, msg, "DEBUG"),
-};
+  public debug(service: string, msg: string): void {
+    this.log(service, msg, "DEBUG");
+  }
+  /**
+   * @public Logs a message to the console using the runtime's default console implementation with the **TRACE** Level
+   *
+   * @param service Service that emitted the log call
+   * @param msg Message to log
+   */
+  public trace(service: string, msg: string): void {
+    this.log(service, msg, "TRACE");
+  }
+}
